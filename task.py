85a5bff088c10a3df5d82fac991dc050a21814a4
Parvez-md-gif <zaynmalik31779@gmail.com>
2025-05-08 11:32:14 +0530

    Add files via upload

--- task.py
+++ task.py
@@ -1,86 +1,42 @@
-# a = 5
-# b = 6
-# print(a + b)      addiction
-# print(a - b)      subtraction
-# print(a * b)      multiply
-# print(a / b)      division
-# print(a // b)     floor division
-# print(a ** b)     power
-# print(a % b)      modulas
+#from app import sentence
 
-# a = 5
-# b = 4
-# print(a > b)
-# print(a < b)
-# print(a >= b)
-# print(a <= b)
-# print(a == b)
+# Firstname = str(input("Enter your first name: "))
+# Lastname = str(input("Enter your last name: "))
+# print(f"Hello, {Firstname} {Lastname}")
 
-# a,b,c = True,False,True
-# if a and c:
-#     print("this are equal to each other")
+#Ask the user to enter a sentence, and then print:
+# sentence = str(input("Enter a sentence: "))
+# print("original",sentence)
+# print("uppercase",sentence.upper())
+# print("lowercase",sentence.lower())
+# print("lenght",len(sentence.replace(" ", "")))
+
+#Ask the user to enter a word or sentence, and check whether it is a palindrome or not.
+# word = str(input("Enter a word: "))
+# if word == word[::-1]:
+#     print("yes it is palindrome")
 # else:
-#     print("this are not equal only one value is true")
-#
-# if b or c:
-#     print("this are only one value true ")
-# else:
-#     print("this are false equal to each other")
-#
-# if not b:
-#     print('true')
-# else:
-#     print('false')
+#     print("no it is not palindrome")
+# print(word.replace(" ", "").lower())
 
-#
-# a = 10
-# b = 10
-# c = -10
-#
-# if a == b :
-#     print(" both are same")
-# if a == c :
-#     print(" both are same")
-# else:
-#     print("atleast one is nagative")
+#Ask the user to enter their full name, and display:
+Firstname = str(input("Enter your first name: "))
+Lastname = str(input("Enter your last name: "))
+print(Firstname.replace(" ", "").split(),Lastname.replace(" ", "").split())
 
-# a = 10
-#
-# if a % 3 ==0 or a % 5 == 0:
-#     print("The number is divisible by 3 either 5")
-# else:
-#     print("The number not divisible")
 
-# def order(x):
-#     print("the value is: ",x)     /important pratice
-#     return True if x > 0 else False
-# a = order
-# b = order
-# c = order
-# d = order
-# e = order
-# if a(-4) or  b(4) or c(10):
-#         print("atleast one of the values is possitive")
 
-# or operator with if
-# def fun(a):
-#     if a % 5 == 0 or a % 10 == 0:
-#        print(" a is multiply")
-#     else:
-#         print(" a is not is multiply")
-# fun(5)
-# fun(8)
-# fun(16)
 
-# a = 5
-# b = 8
-# a += b
-# print(a)
 
-# #ternary operator
-# a = 5
-# b = 10
-# print("a is greater than b"if a>b else "b is greater than a")
 
-nice_weather = True
-print("go to walk"if nice_weather else "watch movie  at home")
+
+
+
+
+
+
+
+
+
+
+
